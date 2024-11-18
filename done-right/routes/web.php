17548fc9c91c3390a3b784621689adf0ce27e25e
Jaycee <jccantos1212@gmail.com>
2024-11-18 18:26:29 +0800

    feat: add new route for practice

--- done-right/routes/web.php
+++ done-right/routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/hello', function () {
+    return 'Hello';
+})->name('hello');
+
+Route::get('/hallo', function () {
+    return redirect()->route('hello');
+});
+
+Route::get('/greet/{name}', function ($name) {
+    // return 'Hello ' . $name . '!';
+    return "Hello $name!";
+});
+
+Route::fallback(function () {
+    return 'What???';
+});
