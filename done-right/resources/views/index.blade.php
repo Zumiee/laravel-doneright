8fe8eb1b3ab127c9e7ccb587b161c088002c3435
Jaycee <jccantos1212@gmail.com>
2024-11-19 12:19:13 +0800

    feat: add blade directives

--- done-right/resources/views/index.blade.php
+++ done-right/resources/views/index.blade.php
@@ -1,5 +1,17 @@
-Hello dayum!
+<h1>
+    The task list!
+</h1>
 
-@isset($name)
-    <div>The name is: {{ $name }}</div>
-@endisset
+<div>
+    @forelse ($tasks as $task)
+        <div>
+            {{-- <a href="{{ route('tasks.show', ['id', ]) }}"></a> --}}
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+            <div>Description: {{ $task->description }}</div>
+        </div>
+    @empty
+        <div>There are no tasks!</div>
+    @endforelse
+</div>
+
+
